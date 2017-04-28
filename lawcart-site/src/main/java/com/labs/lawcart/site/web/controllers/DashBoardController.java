958a12156bf2de728d2a166020a5fb1d000740ac
sreejith <sreejithcools@gmail.com>
2017-04-28 15:40:53 +0530

    update matter

--- lawcart-site/src/main/java/com/labs/lawcart/site/web/controllers/DashBoardController.java
+++ lawcart-site/src/main/java/com/labs/lawcart/site/web/controllers/DashBoardController.java
@@ -15,10 +15,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+
 import com.labs.lawcart.customers.CustomerService;
 import com.labs.lawcart.entities.Customer;
 import com.labs.lawcart.entities.Matter;
 import com.labs.lawcart.entities.Order;
+import com.labs.lawcart.entities.Product;
 import com.labs.lawcart.matter.MatterService;
 
 @Controller
@@ -29,7 +31,7 @@ public class DashBoardController extends LawCartSiteBaseController{
 	@Autowired private MatterService matterService;
 	@Autowired protected PasswordEncoder passwordEncoder;
 	
-	@Override
+	@Override 
 	protected String getHeaderTitle()
 	{
 		return "dashboard/pages/index";
@@ -76,10 +78,21 @@ public class DashBoardController extends LawCartSiteBaseController{
 	}
 	
 	@RequestMapping(value="/matters/{matterId}", method=RequestMethod.GET)
-	public String viewOrder(@PathVariable(value="matterId")Integer matterId, Model model)
+	public String editMatter(@PathVariable(value="matterId")Integer matterId, Model model)
 	{
-		Matter matter = matterService.getMatter(matterId);
+		Matter matter = matterService.getMatterById(matterId);
 		model.addAttribute("matter", matter);
 		return "dashboard/pages/edit_matter";
+	}
+	
+	@RequestMapping(value="/matters/{id}", method=RequestMethod.POST)
+	public String updateMatter(@Valid @ModelAttribute("matter") Matter matter, BindingResult result, 
+			Model model, RedirectAttributes redirectAttributes) {
+		
+		Matter persistedMatter = matterService.updateMatter(matter);
+		
+		logger.debug("Updated matter with id : {} and description : {}", persistedMatter.getId(), persistedMatter.getDescription());
+		redirectAttributes.addFlashAttribute("info", "Matter updated successfully");
+		return "dashboard/pages/matters";
 	}
 }
